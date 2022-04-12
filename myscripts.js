0c17d194fa70952617eb46dbf26f65259ff96997
elena-bollini <77729112+elena-bollini@users.noreply.github.com>
2022-04-12 16:51:40 +0100

    second edition

--- myscripts.js
+++ myscripts.js
@@ -1,26 +1,56 @@
-//login validation
-funcion validateForm(){
+//login validations
+function validateForm(){
   let x=document.forms["login-form"]["username"].value;
+  let y=document.forms["login-form"]["password"].value;
   if (x==null || x==""){
     alert("Username must be filled out");
-  } if (password ==null || password.length != 9){
-    alert("Password must be 9 character")
+  }else if (y ==null || y.length !== 9){
+    alert("Password must be 9 character");
+    return false;
+  } else {alert("You logger in");}
+}
+
+
+//alert health warning
+function getItem(item){
+  let value = item.value;
+
+  if(value == "supersize"){
+    alert("Mind your health!");
   }
 }
 
+//addBurger
+function addBurger(){
+  let x=document.forms["menu-form"]["myDropdownItems"].value;
+  let price=0;
+  let y=document.forms["menu-form"]["quantity"].value;
+  if(x=="large"){ price=4;}
+  if(x=="extralarge"){price=6;}
+  if(x=="supersize"){price=10;}
+  let result = price*y;
 
-// select Burgers
-function showBurgers() {
-  var x = document.getElementById("myDropdownItems").value;
-  document.getElementById("item").innerHTML = "You selected: " + x;
+  document.getElementById("totalPrice").innerHTML=result;
 }
 
-//alert health warning
-function msgHealth() {
-  alert("Mind your health!");
+//insert discount
+function getDiscount(){
+  let x=document.forms["discount-form"]["discount"].value;
+  let discountPrice=document.getElementById("totalPrice").innerHTML;
+  //let totalPrice = document.forms["discount-form"]["totalPrice"].value;
+    if (x=="shillelagh"){
+    discountPrice = discountPrice*0.9;
+    alert("You got 10% of discount! Total price: " + discountPrice + "euro");
+  } else {alert("Your discount code is incorrect")}
 }
 
-funcion totalPrice(burgerSize, quantity){
-  burgerSize * quantity
 
-}
+
+//confetti explosion under thw white rabbit
+  $('.whiteBunny').on('click', () => {
+     $('.show-discount').show();
+ });
+
+ $('.whiteBunny').on('click', () => {
+   $('.whiteBunny').confettiButton();
+});
